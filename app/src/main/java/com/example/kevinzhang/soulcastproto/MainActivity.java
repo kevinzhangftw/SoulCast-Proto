947d4cf7e8d7514ab42824f444c4483c704d6cc0
Ambar <ambar.dhesi@gmail.com>
2016-11-10 13:37:26 -0800

    Add permissions library.

--- app/src/main/java/com/example/kevinzhang/soulcastproto/MainActivity.java
+++ app/src/main/java/com/example/kevinzhang/soulcastproto/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.kevinzhang.soulcastproto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    void setUpLocation(){
+
+    }
+
+    @NeedsPermission(Manifest.permission.RECORD_AUDIO)
+    void setUpAudioRecorder(){
+
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void setUpStorage(){
+
+    }
+
+    @OnPermissionDenied(Manifest.permission.ACCESS_FINE_LOCATION)
+    void showLocationPermissionDenied(){
+
+    }
+
+    @OnPermissionDenied(Manifest.permission.RECORD_AUDIO)
+    void showAudioPermissionDenied(){
+
+    }
+
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showStoragePermissionDenied(){
+
+    }
+
 }
