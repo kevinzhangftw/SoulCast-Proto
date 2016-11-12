7b8e7da43d832cf66b5a1061e7c3f29d2cf93db8
Ambar <ambar.dhesi@gmail.com>
2016-11-12 14:43:34 -0800

    Add listener for Record Button

--- app/src/main/java/com/example/kevinzhang/soulcastproto/HomeActivity.java
+++ app/src/main/java/com/example/kevinzhang/soulcastproto/HomeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -39,6 +41,21 @@ GoogleApiClient.OnConnectionFailedListener,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mRecordButton = (Button) findViewById(R.id.record_button);
+        mRecordButton.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        // User pressed down on the button
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // User released the button
+                        break;
+                }
+                return false;
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
             .findFragmentById(R.id.map);
